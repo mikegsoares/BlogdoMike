c6c99aaf248ca8016d4a5c925112d2af354fad58
Mike Soares <mike.gsoares@sp.senac.br>
2018-04-10 11:35:18 -0300

    Alterações da Controller para Inserir Imagens

--- BlogdoMike/Controllers/PostsController.cs
+++ BlogdoMike/Controllers/PostsController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BlogdoMike.Models;
+using BlogdoMike.ViewModels;
 
 namespace BlogdoMike.Controllers
 {
@@ -14,9 +16,26 @@ namespace BlogdoMike.Controllers
         // GET: Posts
         public ActionResult Index()
         {
-            var posts = _context.Posts.ToList();
+            var content = _context.Posts.Select(s => new
+            {
+                s.Id,
+                s.Title,
+                s.DateAdded,
+                s.DateUpdated,
+                s.Image,
+                s.PostingBody
+            });
+            var viewModel = content.Select(p => new PostViewModel()
+            {
+                Id = p.Id,
+                Title = p.Title,
+                DateAdded = p.DateAdded,
+                DateUpdated = p.DateUpdated,
+                Image = p.Image,
+                PostingBody = p.PostingBody
+            }).ToList();
 
-            return View(posts);
+            return View(viewModel);
         }
 
         public ActionResult Details(int id)
@@ -68,23 +87,89 @@ namespace BlogdoMike.Controllers
         }
 
         [HttpPost]
-        public ActionResult Save(Post post)
+        public ActionResult Save(PostViewModel post)
         {
+            var file = Request.Files["ImageData"];
+
             if (post.Id == 0)
             {
+                var service = new PostsController();
+                var i = service.UploadImageInDb(file, post);
+                if (i == 1)
+                {
+                    return RedirectToAction("Index");
+                }
+
                 post.DateAdded = DateTime.Now;
-                _context.Posts.Add(post);
+              //  _context.Posts.Add(post); - Removido!
             }
             else
             {
                 var postInDb = _context.Posts.Single(p => p.Id == post.Id);
                 postInDb.Title = post.Title;
                 postInDb.DateUpdated = DateTime.Now;
+                // postInDb.Image = post.Image; 
                 postInDb.PostingBody = post.PostingBody;
+
+                if (file.ContentLength > 0)
+                {
+                    postInDb.Image = ConvertToBytes(file);
+                }
+                else
+                {
+                    _context.Entry(postInDb).Property(i => i.Image).IsModified = false;
+                }
             }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Posts");
         }
+
+        public ActionResult RetrieveImage(int id)
+        {
+            var cover = GetImageFromDatabase(id);
+            if (cover != null)
+            {
+                return File(cover, "image/png");
+            }
+            else
+            {
+                return null;
+            }
+         //   return cover != null ? File(cover, "image/png") : null;
+        }
+
+
+        public byte[] GetImageFromDatabase(int id)
+        {
+            var q = _context.Posts.Where(i => i.Id == id).Select(i => i.Image);
+            var cover = q.First();
+            return cover;
+        }
+
+        public int UploadImageInDb(HttpPostedFileBase file, PostViewModel viewModel)
+        {
+            viewModel.Image = ConvertToBytes(file);
+            var post = new Post
+            {
+                Title = viewModel.Title,
+                DateAdded = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                PostingBody = viewModel.PostingBody,
+                Image = viewModel.Image
+            };
+
+            _context.Posts.Add(post);
+            var i = _context.SaveChanges();
+            return i == 1 ? 1 : 0;
+        }
+
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            var reader = new BinaryReader(image.InputStream);
+            var imageBytes = reader.ReadBytes(image.ContentLength);
+            return imageBytes;
+        }
+
     }
 }
---
+++ BlogdoMike/ViewModels/PostViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogdoMike.ViewModels
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        [AllowHtml]
+        public string PostingBody { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
